d752d182b837a97354943914f1922fac760c9b19
Rian Wright <wrightr1@arizona.edu>
2024-02-27 19:05:34 +0000

    Amended mapshot 2 occurence map

--- Occurence map.R
+++ Occurence map.R
@@ -2,17 +2,30 @@ data <- read.csv("data/cleanedData.csv")
 
 library(leaflet)
 library(mapview)
+library(webshot2)
 
-map <- leaflet() %>%
-  addProviderTiles("Esri.WorldTopoMap") %>%
+# Generate occurrence map
+map <- leaflet(options = leafletOptions(zoomControl = FALSE)) %>% 
+# The options bit removes the zoom button
+
+# Add base map
+# More options here: https://leaflet-extras.github.io/leaflet-providers/preview/
+  addProviderTiles("Esri.WorldTopoMap") %>% 
+
+# Add occurrence points
   addCircleMarkers(data = data,
                    lat = ~decimalLatitude,
                    lng = ~decimalLongitude,
                    radius = 3,
                    color = "coral",
                    fillOpacity = 0.8) %>%
-  addLegend(position = "topright",
+ 
+  # Add legend/title
+   addLegend(position = "topright",
             title = "Species Occurences from GBIF",
             labels = "Habronattus americanus",
             colors = "coral",
             opacity = 0)
+
+# Save the map
+mapshot2(map, file = "output/leafletTest.png")
